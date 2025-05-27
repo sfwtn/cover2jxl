2326bf97edcb93c1210957e2be9ffb1255ab86a9
sfwtn <121581681+sfwtn@users.noreply.github.com>
2025-05-27 22:09:52 +0300

    Upped res to 1400p

--- cover2jxl.py
+++ cover2jxl.py
@@ -12,29 +12,29 @@ extension = file.split(".")[-1]
 # WebP route
 if extension=="webp":
     print("Detected WebP format which cannot be converted due to libjxl's refusal to support it. Opting for other formats will result in larger file sizes. Proceeding without JpegXl.")
-    os.system(f"magick convert cover.webp -resize 1080x1080 cover-1080p.jpeg")
+    os.system(f"magick convert cover.webp -resize 1400x1400 cover-1400p.jpeg")
 
     originalFile = os.stat("cover.webp").st_size / 1024 /1024
-    jpegFile = os.stat("cover-1080p.jpeg").st_size / 1024 /1024
-    print(f"Done:\nOriginal file:\t{originalFile:.2f} MB\n1080p file:\t{jpegFile:.2f} MB")
+    jpegFile = os.stat("cover-1400p.jpeg").st_size / 1024 /1024
+    print(f"Done:\nOriginal file:\t{originalFile:.2f} MB\n1400p file:\t{jpegFile:.2f} MB")
     exit(0)
 # AVIF route
 elif extension=="avif":
     print("Detected AVIF format which cannot be converted due to libjxl's refusal to support it. Opting for other formats will result in larger file sizes. Proceeding without JpegXl.")
-    os.system(f"magick convert cover.avif -resize 1080x1080 cover-1080p.jpeg")
+    os.system(f"magick convert cover.avif -resize 1400x1400 cover-1400p.jpeg")
 
     originalFile = os.stat("cover.avif").st_size / 1024 /1024
-    jpegFile = os.stat("cover-1080p.jpeg").st_size / 1024 /1024
-    print(f"Done:\nOriginal file:\t{originalFile:.2f} MB\n1080p file:\t{jpegFile:.2f} MB")
+    jpegFile = os.stat("cover-1400p.jpeg").st_size / 1024 /1024
+    print(f"Done:\nOriginal file:\t{originalFile:.2f} MB\n1400p file:\t{jpegFile:.2f} MB")
     exit(0)
 # JXL route
 elif extension=="jxl":
     print("Detected JpegXL format. Proceeding without conversion.")
-    os.system(f"magick convert cover.jxl -resize 1080x1080 cover-1080p.jpeg")
+    os.system(f"magick convert cover.jxl -resize 1400x1400 cover-1400p.jpeg")
 
     originalFile = os.stat("cover.jxl").st_size / 1024 /1024
-    jpegFile = os.stat("cover-1080p.jpeg").st_size / 1024 /1024
-    print(f"Done:\nOriginal file:\t{originalFile:.2f} MB\n1080p file:\t{jpegFile:.2f} MB")
+    jpegFile = os.stat("cover-1400p.jpeg").st_size / 1024 /1024
+    print(f"Done:\nOriginal file:\t{originalFile:.2f} MB\n1400p file:\t{jpegFile:.2f} MB")
     exit(0)
 
 os.system(f"cp cover.{extension} coverBKP.{extension}")
@@ -43,12 +43,12 @@ os.system(f"magick mogrify -strip cover.{extension}")   #metadata strip, actuall
 
 os.system(f"cjxl cover.{extension} -q 100 -e 9 cover.jxl")
 
-os.system(f"magick convert cover.{extension} -resize 1080x1080 cover-1080p.jpeg")
+os.system(f"magick convert cover.{extension} -resize 1400x1400 cover-1400p.jpeg")
 
 os.remove(f"cover.{extension}")
 
 originalFile = os.stat("coverBKP." + extension).st_size / 1024 /1024
 jxlFile = os.stat("cover.jxl").st_size / 1024 /1024
-jpegFile = os.stat("cover-1080p.jpeg").st_size / 1024 /1024
+jpegFile = os.stat("cover-1400p.jpeg").st_size / 1024 /1024
 
-print(f"Done:\nOriginal file:\t{originalFile:.2f} MB\nJpegXl file:\t{jxlFile:.2f} MB\n1080p file:\t{jpegFile:.2f} MB")
+print(f"Done:\nOriginal file:\t{originalFile:.2f} MB\nJpegXl file:\t{jxlFile:.2f} MB\n1400p file:\t{jpegFile:.2f} MB")
